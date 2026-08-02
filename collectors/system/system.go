5bc3bdc35d69b6535934c1cf8a35604c0c6b7187
Shihab <shihabud121@outlook.com>
2026-08-02 23:03:45 +0600

    feat(collector): collect system hostname

--- collectors/system/system.go
+++ collectors/system/system.go
@@ -1,6 +1,9 @@
 package system
 
 import (
+	"os"
+	"strings"
+
 	"github.com/Shihab369/devops-thinking-lab/internal/core"
 	"github.com/Shihab369/devops-thinking-lab/internal/models"
 )
@@ -12,5 +15,17 @@ var _ core.Collector = SystemCollector{}
 func (s SystemCollector) Collect() models.Result {
 	return models.Result{
 		Name: "system",
+		Data: map[string]interface{}{
+			"hostname": readHostname(),
+		},
 	}
+}
+
+func readHostname() string {
+	data, err := os.ReadFile("/proc/sys/kernel/hostname")
+	if err != nil {
+		return ""
+	}
+
+	return strings.TrimSpace(string(data))
 }

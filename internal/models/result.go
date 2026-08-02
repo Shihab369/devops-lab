94dc107bb71e307dc2320ebb8f4d6d89fae556f8
Shihab <shihabud121@outlook.com>
2026-08-02 22:51:43 +0600

    feat(models): extend result with collector data

--- internal/models/result.go
+++ internal/models/result.go
@@ -1,10 +1,6 @@
 package models
 
-import "time"
-
 type Result struct {
-	Name      string      `json:"name"`
-	Status    string      `json:"status"`
-	Timestamp time.Time   `json:"timestamp"`
-	Data      interface{} `json:"data"`
+	Name string
+	Data map[string]interface{}
 }

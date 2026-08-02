c5d09aebbc887147b8c54fe26882a80aeabdcba5
Shihab <shihabud121@outlook.com>
2026-08-02 23:13:53 +0600

    feat(collector): collect kernel information

--- collectors/system/system.go
+++ collectors/system/system.go
@@ -17,12 +17,22 @@ func (s SystemCollector) Collect() models.Result {
 		Name: "system",
 		Data: map[string]interface{}{
 			"hostname": readHostname(),
+			"kernel":   readKernel(),
 		},
 	}
 }
 
 func readHostname() string {
 	data, err := os.ReadFile("/proc/sys/kernel/hostname")
+	if err != nil {
+		return ""
+	}
+
+	return strings.TrimSpace(string(data))
+}
+
+func readKernel() string {
+	data, err := os.ReadFile("/proc/version")
 	if err != nil {
 		return ""
 	}

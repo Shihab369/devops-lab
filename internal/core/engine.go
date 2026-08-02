9e16be81a5bbe68f054577a572920f08963339ca
Shihab <shihabud121@outlook.com>
2026-08-02 21:19:23 +0600

    feat(core): add diagnostic engine skeleton

---
+++ internal/core/engine.go
@@ -0,0 +1,21 @@
+package core
+
+import "github.com/Shihab369/devops-thinking-lab/internal/models"
+
+type Engine struct {
+	results []models.Result
+}
+
+func NewEngine() *Engine {
+	return &Engine{
+		results: make([]models.Result, 0),
+	}
+}
+
+func (e *Engine) AddResult(result models.Result) {
+	e.results = append(e.results, result)
+}
+
+func (e *Engine) Results() []models.Result {
+	return e.results
+}

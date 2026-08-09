f32895d2f74f67ab9ae4b5524dfa97b4945cb4ae
Shihab <shihabud121@outlook.com>
2026-08-09 22:29:39 +0600

    feat: add network diagnostics

--- collectors/network/network_test.go
+++ collectors/network/network_test.go
@@ -1,6 +1,9 @@
 package network
 
-import "testing"
+import (
+	"net"
+	"testing"
+)
 
 func TestListInterfaces(t *testing.T) {
 	interfaces := listInterfaces()
@@ -25,6 +28,14 @@ func TestReadInterfaceInfo(t *testing.T) {
 
 	if info.mtu == 0 {
 		t.Fatal("expected positive MTU")
+	}
+
+	if info.ipv4Addresses == nil {
+		t.Fatal("expected IPv4 addresses slice")
+	}
+
+	if info.ipv6Addresses == nil {
+		t.Fatal("expected IPv6 addresses slice")
 	}
 }
 
@@ -66,5 +77,44 @@ func TestNetworkCollector(t *testing.T) {
 		if _, ok := info["tx_bytes"]; !ok {
 			t.Fatalf("expected tx_bytes for interface %s", name)
 		}
+	}
+}
+
+func TestSplitAddresses(t *testing.T) {
+	addrs := []net.Addr{
+		&net.IPNet{
+			IP:   net.ParseIP("192.168.1.10"),
+			Mask: net.CIDRMask(24, 32),
+		},
+		&net.IPNet{
+			IP:   net.ParseIP("10.0.0.5"),
+			Mask: net.CIDRMask(8, 32),
+		},
+		&net.IPNet{
+			IP:   net.ParseIP("fe80::1"),
+			Mask: net.CIDRMask(64, 128),
+		},
+		&net.IPNet{
+			IP:   net.ParseIP("2001:db8::1"),
+			Mask: net.CIDRMask(64, 128),
+		},
+	}
+
+	ipv4, ipv6 := splitAddresses(addrs)
+
+	if len(ipv4) != 2 {
+		t.Fatalf("expected 2 IPv4 addresses, got %d", len(ipv4))
+	}
+
+	if len(ipv6) != 2 {
+		t.Fatalf("expected 2 IPv6 addresses, got %d", len(ipv6))
+	}
+
+	if ipv4[0] != "192.168.1.10/24" {
+		t.Fatalf("unexpected IPv4 address: %s", ipv4[0])
+	}
+
+	if ipv6[0] != "fe80::1/64" {
+		t.Fatalf("unexpected IPv6 address: %s", ipv6[0])
 	}
 }
